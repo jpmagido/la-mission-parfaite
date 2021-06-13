8581989dacac8a060c2b22814d95345ba27eb353
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2021-06-13 12:13:14 +0200

    push to stream

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,16 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def search
+    if params[:search].blank?
+      redirect_to restaurants_path
+    else
+      parameter = params[:search].downcase
+      @results = Restaurant.where(name: parameter)
+      # @results = Restaurant.all.where("lower(name) LIKE :search OR lower(restaurant.city) LIKE :search", search: "%#{parameter}%")
+    end
+  end
+
 end
+
+
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,4 +6,5 @@ class Restaurant < ApplicationRecord
 
   belongs_to :city
   belongs_to :location
+
 end
