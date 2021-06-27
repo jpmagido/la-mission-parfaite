253f44b7946e0d55c3fc4a62b4c89f974af9c07d
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2021-06-27 16:52:40 +0200

    add module helper is_vegan + spec + delete seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    Location.destroy_all
+    Location.create!(
+      address: 'rue du moulin',
+      street_number: '30',
+      zip_code: '83400',
+      city_id: 4
+      )
+    Location.create!(
+      address: 'avenue Carmen',
+      street_number: '75',
+      zip_code: '35200',
+      city_id: 2
+    )
+    Restaurant.destroy_all
+    Restaurant.create!(
+      name: 'la galinette',
+      kitchen: 'cuisine du Sud-Ouest',
+      vegan: false,
+      city_id: 3,
+      location_id: 1
+      )
+    Restaurant.create!(
+      name: 'La voile bleue',
+      kitchen: 'cuisine du marché',
+      vegan: true,
+      city_id: 2,
+      location_id: 2
+    )
+
--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Restaurants", type: :request do
+    let(:restaurant) { create(:restaurant) }
   describe "GET /show" do
     it "returns http success" do
-      get "/restaurants/show"
+      get restaurant_path restaurant
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /index" do
     it "returns http success" do
-      get "/restaurants/index"
+      get restaurants_path
       expect(response).to have_http_status(:success)
     end
   end
