83693d73dbfdc3b12f9ada21fe9ad4b3a9be26d6
jpmagido <jpmagido@me.com>
2021-10-21 19:06:31 +0200

    fix restaurant search

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,8 +4,16 @@ class RestaurantsController < ApplicationController
 
   private
 
+  def restaurants
+    @restaurants ||= params['restaurant'] ? city_restaurants.where(query) : Restaurant.all.limit(100)
+  end
+
+  def restaurant
+    @restaurant ||= restaurants.find(params[:id])
+  end
+
   def city_restaurants
-    @city_restaurants ||= City.find_by(id: params['Restaurant'][:city_id])&.restaurants || Restaurant.all
+    @city_restaurants ||= City.find_by(id: params['restaurant'][:city_id])&.restaurants || Restaurant.all
   end
 
   def query
@@ -15,32 +23,9 @@ class RestaurantsController < ApplicationController
     query
   end
 
-  def restaurants
-    @restaurants ||=
-      begin
-        if params['Restaurant']
-          city_restaurants.where query
-        else
-          Restaurant.where query
-        end
-      end
-  end
-
-  def restaurant
-    @restaurant ||= restaurants.find(params[:id])
-  end
-
   def name
     @name ||= params[:name].downcase
   end
-
-  # def valid_params
-  #   restaurant_params.delete_if { |_, value| value.empty? }
-  # end
-
-  # def restaurant_params
-  #   params.permit(:name, :vegan, city_restaurants: {})
-  # end
 end
 
 
