0dfb536b6f6924664a66281b53db5e10cabc20ec
seven-dev7 <seven.tdr7@gmail.com>
2021-10-25 16:59:38 +0200

    not finish spec request

---
+++ spec/requests/administration/admin_messages_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Administration::AdminMessages', type: :request do
+  let(:admin) { create(:admin) }
+  let(:restaurant) { create(:restaurant) }
+  let(:admin_review) { 
+    create(
+      :admin_review, 
+      reviewable: restaurant,
+      admin_id: admin
+    ) 
+  }
+
+  describe 'GET /index' do
+    it 'returns http success' do
+      get administration_admin_messages_url
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get administration_admin_message_url admin_review
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
