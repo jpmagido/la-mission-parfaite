1a046c708e5556389e84ff75817d1bd3a75ed6ca
seven-dev7 <seven.tdr7@gmail.com>
2021-10-26 08:44:31 +0200

    fix housing controller with index view

--- app/controllers/housings_controller.rb
+++ app/controllers/housings_controller.rb
@@ -3,22 +3,18 @@
 class HousingsController < ApplicationController
   helper_method :housing, :housings
 
-  def index
-    @housings ||= Housing.search_and_paginate(params, per_page: 20).order(updated_at: :desc)
-  end
-
   private
 
   def params_housing
     params.require(:housing).permit(:breakfast, :url, :housing_type)
   end
   
-  #def housings
-  #  @housings ||= Housing.all
-  #end
+  def housings
+    @housings ||= Housing.search_and_paginate(params, per_page: 20).order(updated_at: :desc)
+  end
   
   def housing
     @housing ||= Housing.find params[:id]
   end
-  
+
 end
