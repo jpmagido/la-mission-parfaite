7b3ff4762f022cd354ab486ef4e3b471ef6694b0
seven-dev7 <seven.tdr7@gmail.com>
2021-10-26 09:40:10 +0200

    refacto let in admin_messages spec and begin test for filtering not working

--- spec/requests/administration/admin_messages_spec.rb
+++ spec/requests/administration/admin_messages_spec.rb
@@ -5,13 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Administration::AdminMessages', type: :request do
   let(:admin) { create(:admin) }
   let(:restaurant) { create(:restaurant) }
-  let(:admin_review) { 
-    create(
-      :admin_review, 
-      reviewable: restaurant,
-      admin_id: admin
-    ) 
-  }
+  let(:admin_review) { create(:admin_review, reviewable: restaurant, admin_id: admin, status: 1) }
 
   describe 'GET /index' do
     it 'returns http success' do
@@ -24,6 +18,14 @@ RSpec.describe 'Administration::AdminMessages', type: :request do
     it 'returns http success' do
       get administration_admin_message_url admin_review
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /search' do
+    context 'when filtering by status' do
+      it "returns only Admin Messages with the provided :status" do
+        expect(admin_review.status).to eq('todo')
+      end
     end
   end
 end
