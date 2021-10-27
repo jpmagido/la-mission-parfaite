57a9391bb518855a9fe12b6a904e074f1b89d0d4
GlenDu <glen_llaci@yahoo.com>
2021-10-27 12:44:30 +0200

    Creation card for cities, show city, creat def in model to count Restaurants and housings by city

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,16 @@
 class City < ApplicationRecord
   has_many :locations
   has_many :restaurants
-  
+  has_many :housings
+
   validates :name, presence: true
   validates_length_of :name, in: 3..20
+
+  def count_restaurants
+    Restaurant.where(:city_id => self.id).count
+  end
+
+  def count_housings
+    Housing.where(:city_id => self.id).count
+  end  
 end
