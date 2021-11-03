8a7f6bffe13d6c710d8b5b254111a4396a1d62e5
GlenDu <glen_llaci@yahoo.com>
2021-11-03 17:59:58 +0100

    Reglage end line, title card, bordure,button show to details

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,12 +7,4 @@ class City < ApplicationRecord
 
   validates :name, presence: true
   validates_length_of :name, in: 3..20
-
-  def count_restaurants
-    Restaurant.where(:city_id => self.id).count
-  end
-
-  def count_housings
-    Housing.where(:city_id => self.id).count
-  end  
 end
